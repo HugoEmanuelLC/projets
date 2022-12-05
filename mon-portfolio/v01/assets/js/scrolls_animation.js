04fa7333f377162013d08baa62fc08a71322df2c
hugo clavinas <helc85.info@gmail.com>
2022-12-05 14:36:39 +0100

    projets portfolio v01

--- mon-portfolio/v01/assets/js/scrolls_animation.js
+++ mon-portfolio/v01/assets/js/scrolls_animation.js
@@ -3,13 +3,36 @@
 // BIBLIOTHEQUE JQUERY
 $(function(){
     let html_body = $('html, body');
+    let contact = $('.contact');
+    let objectifs = $('.objectifs');
+    let competences = $('.competences');
     let footer = $('footer');
+
     let a_scroll_up = $(".a_scroll_up");
+    let a_contact = $(".a_contact");
+    let a_objectifs = $(".a_objectifs");
+    let a_competences = $(".a_competences");
     let a_scroll_down = $(".a_scroll_down");
 
     a_scroll_up.click(function(){
         html_body.animate({
             scrollTop: html_body.offset().top
+        }, 2000);
+    })
+    
+    a_contact.click(function(){
+        html_body.animate({
+            scrollTop: contact.offset().top
+        }, 2000);
+    })
+    a_objectifs.click(function(){
+        html_body.animate({
+            scrollTop: objectifs.offset().top
+        }, 2000);
+    })
+    a_competences.click(function(){
+        html_body.animate({
+            scrollTop: competences.offset().top
         }, 2000);
     })
     a_scroll_down.click(function(){
