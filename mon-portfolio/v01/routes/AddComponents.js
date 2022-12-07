f23c00c7f6ef69d6eb8bbe8e78820f58375ea885
hugo <helc85.info@gmail.com>
2022-12-07 23:01:03 +0100

    developpement mon portfolio v1

--- mon-portfolio/v01/routes/AddComponents.js
+++ mon-portfolio/v01/routes/AddComponents.js
@@ -8,6 +8,7 @@ var routes = [
     {path: "#/notfound", name: "notfound", component: "errorPage.html"},
     {path: "#/", name: "home", component: "home.html"},
     {path: "#/home", name: "home", component: "home.html"},
+    {path: "#/todolist", name: "todolist", component: "toDoListPage.html"},
     {path: "#/pagetest", name: "page", component: "pageTest.html"},
 
     // mode target
