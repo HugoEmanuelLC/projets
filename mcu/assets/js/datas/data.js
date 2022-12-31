4b9be40d247b337363c801cf6c1a541aad68b528
hugo <helc85.info@gmail.com>
2022-12-31 16:07:25 +0100

    app mcu

---
+++ mcu/assets/js/datas/data.js
@@ -0,0 +1,51 @@
+const allMcuMoviesSeries = [
+    {
+        name: "Avengers",
+        type: "movie",
+        date: 2012,
+        image: "https://wallpapercave.com/wp/vDy3sUb.jpg",
+        phase: 1,
+        personages: [
+            {name: "iron man"},
+            {name: "captain america"},
+            {name: "thor"},
+        ]
+    },
+    {
+        name: "Avengers l'ere de ultron",
+        type: "movie",
+        date: 2015,
+        image: "https://wallpapercave.com/wp/wp1818445.jpg",
+        phase: 2,
+        personages: [
+            {name: "iron man"},
+            {name: "captain america"},
+            {name: "thor"},
+        ]
+    },
+    {
+        name: "Avengers infinity war",
+        type: "movie",
+        date: 2018,
+        image: "https://images4.alphacoders.com/909/thumb-1920-909185.jpg",
+        phase: 3,
+        personages: [
+            {name: "iron man"},
+            {name: "captain america"},
+            {name: "thor"},
+        ]
+    },
+    {
+        name: "Avengers endgame",
+        type: "movie",
+        date: 2019,
+        image: "https://images8.alphacoders.com/100/thumb-1920-1003220.png",
+        phase: 3,
+        personages: [
+            {name: "iron man"},
+            {name: "captain america"},
+            {name: "thor"},
+        ]
+    }
+]
+
