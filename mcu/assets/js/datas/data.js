786073b81a57d3af2fda1ae5108f59f394b60c96
hugo clavinas <helc85.info@gmail.com>
2023-01-04 11:23:12 +0100

    projets mcu

--- mcu/assets/js/datas/data.js
+++ mcu/assets/js/datas/data.js
@@ -1,10 +1,11 @@
 const allMcuMoviesSeries = [
+    //AVENGERS
     {
         name: "Avengers",
         type: "movie",
         date: 2012,
         image: "https://wallpapercave.com/wp/vDy3sUb.jpg",
-        phase: 1,
+        phase: "phase1",
         personages: [
             {name: "iron man"},
             {name: "captain america"},
@@ -12,7 +13,9 @@ const allMcuMoviesSeries = [
         ],
         collection:[
             {name: "Avengers"},
-            {name: "iron man"}
+            {name: "iron man"},
+            {name: "captain america"},
+            {name: "thor"},
         ]
     },
     {
@@ -20,7 +23,7 @@ const allMcuMoviesSeries = [
         type: "movie",
         date: 2015,
         image: "https://wallpapercave.com/wp/wp1818445.jpg",
-        phase: 2,
+        phase: "phase2",
         personages: [
             {name: "iron man"},
             {name: "captain america"},
@@ -28,7 +31,9 @@ const allMcuMoviesSeries = [
         ],
         collection:[
             {name: "Avengers"},
-            {name: "iron man"}
+            {name: "iron man"},
+            {name: "captain america"},
+            {name: "thor"},
         ]
     },
     {
@@ -36,7 +41,7 @@ const allMcuMoviesSeries = [
         type: "movie",
         date: 2018,
         image: "https://images4.alphacoders.com/909/thumb-1920-909185.jpg",
-        phase: 3,
+        phase: "phase3",
         personages: [
             {name: "iron man"},
             {name: "captain america"},
@@ -44,7 +49,9 @@ const allMcuMoviesSeries = [
         ],
         collection:[
             {name: "Avengers"},
-            {name: "iron man"}
+            {name: "iron man"},
+            {name: "captain america"},
+            {name: "thor"},
         ]
     },
     {
@@ -52,7 +59,7 @@ const allMcuMoviesSeries = [
         type: "movie",
         date: 2019,
         image: "https://images8.alphacoders.com/100/thumb-1920-1003220.png",
-        phase: 3,
+        phase: "phase3",
         personages: [
             {name: "iron man"},
             {name: "captain america"},
@@ -60,15 +67,19 @@ const allMcuMoviesSeries = [
         ],
         collection:[
             {name: "Avengers"},
-            {name: "iron man"}
+            {name: "iron man"},
+            {name: "captain america"},
+            {name: "thor"},
         ]
     },
+
+    //IRONMAN
     {
         name: "Iron man",
         type: "movie",
         date: 2008,
         image: "https://wallpapercave.com/wp/wp2547005.jpg",
-        phase: 1,
+        phase: "phase1",
         personages: [
             {name: "iron man"},
         ],
@@ -77,16 +88,113 @@ const allMcuMoviesSeries = [
         ]
     },
     {
+        name: "Iron man 2",
+        type: "movie",
+        date: 2010,
+        image: "https://wallpapercave.com/wp/fJxKhs9.jpg",
+        phase: "phase1",
+        personages: [
+            {name: "iron man"},
+        ],
+        collection:[
+            {name: "iron man"}
+        ]
+    },
+    {
+        name: "Iron man 3",
+        type: "movie",
+        date: 2013,
+        image: "https://i.pinimg.com/originals/11/ed/bd/11edbd2b87a4926224f8c5dfa05526e2.jpg",
+        phase: "phase2",
+        personages: [
+            {name: "iron man"},
+        ],
+        collection:[
+            {name: "iron man"}
+        ]
+    },
+
+    //CAPTAINAMERICA
+    {
         name: "Captain America",
         type: "movie",
         date: 2011,
         image: "https://wallpapercave.com/wp/wp3775632.jpg",
-        phase: 1,
+        phase: "phase1",
         personages: [
             {name: "captain america"},
         ],
         collection:[
             {name: "captain america"}
+        ]
+    },
+    {
+        name: "Captain America 2",
+        type: "movie",
+        date: 2014,
+        image: "https://wallpaperaccess.com/full/153821.jpg",
+        phase: "phase2",
+        personages: [
+            {name: "captain america"},
+        ],
+        collection:[
+            {name: "captain america"}
+        ]
+    },
+    {
+        name: "Captain America civil war",
+        type: "movie",
+        date: 2016,
+        image: "https://rare-gallery.com/thumbs/516718-captain-america.jpg",
+        phase: "phase3",
+        personages: [
+            {name: "captain america"},
+            {name: "iron man"},
+        ],
+        collection:[
+            {name: "captain america"},
+            {name: "iron man"},
+        ]
+    },
+
+    //THOR
+    {
+        name: "Thor",
+        type: "movie",
+        date: 2011,
+        image: "https://wallpaperaccess.com/full/645141.jpg",
+        phase: "phase1",
+        personages: [
+            {name: "thor"},
+        ],
+        collection:[
+            {name: "thor"},
+        ]
+    },
+    {
+        name: "Thor 2",
+        type: "movie",
+        date: 2013,
+        image: "https://wallpaperaccess.com/full/1653473.jpg",
+        phase: "phase2",
+        personages: [
+            {name: "thor"},
+        ],
+        collection:[
+            {name: "thor"},
+        ]
+    },
+    {
+        name: "Thor ragnarok",
+        type: "movie",
+        date: 2017,
+        image: "https://preview.redd.it/m51kk6nexfbz.jpg?auto=webp&s=4fdf94d063e08dabcffb6ffef2f584d6ecb62129",
+        phase: "phase3",
+        personages: [
+            {name: "thor"},
+        ],
+        collection:[
+            {name: "thor"},
         ]
     },
 
