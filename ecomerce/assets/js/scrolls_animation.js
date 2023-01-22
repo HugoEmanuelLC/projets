f605bb91f0003a732f0e5fba270824b01ca36486
hugo <helc85.info@gmail.com>
2023-01-22 18:39:28 +0100

    new projet ecomerce

---
+++ ecomerce/assets/js/scrolls_animation.js
@@ -0,0 +1,67 @@
+
+
+// BIBLIOTHEQUE JQUERY
+$(function(){
+
+    // SIBLES
+    // let html_body = $('html, body');
+    // let contact = $('.contact');
+    // let formations = $('.formations');
+    // let objectifs = $('.objectifs');
+    // let competences = $('.competences');
+    // let projets = $('.projets');
+    // let demos = $('.demos');
+    // let footer = $('footer');
+
+    // BTS
+    // let a_scroll_up = $(".a_scroll_up");
+    // let a_contact = $(".a_contact");
+    // let a_formations = $(".a_formations");
+    // let a_objectifs = $(".a_objectifs");
+    // let a_competences = $(".a_competences");
+    // let a_projets = $(".a_projets");
+    // let a_demos = $(".a_demos");
+    // let a_scroll_down = $(".a_scroll_down");
+
+    // a_scroll_up.click(function(){
+    //     html_body.animate({
+    //         scrollTop: html_body.offset().top
+    //     }, 2000);
+    // })
+    
+    // a_contact.click(function(){
+    //     html_body.animate({
+    //         scrollTop: contact.offset().top
+    //     }, 2000);
+    // })
+    // a_formations.click(function(){
+    //     html_body.animate({
+    //         scrollTop: formations.offset().top
+    //     }, 2000);
+    // })
+    // a_objectifs.click(function(){
+    //     html_body.animate({
+    //         scrollTop: objectifs.offset().top
+    //     }, 2000);
+    // })
+    // a_competences.click(function(){
+    //     html_body.animate({
+    //         scrollTop: competences.offset().top
+    //     }, 2000);
+    // })
+    // a_projets.click(function(){
+    //     html_body.animate({
+    //         scrollTop: projets.offset().top
+    //     }, 2000);
+    // })
+    // a_demos.click(function(){
+    //     html_body.animate({
+    //         scrollTop: demos.offset().top
+    //     }, 2000);
+    // })
+    // a_scroll_down.click(function(){
+    //     html_body.animate({
+    //         scrollTop: footer.offset().top
+    //     }, 2000);
+    // })
+})
